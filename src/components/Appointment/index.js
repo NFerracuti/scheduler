760406561150f2776888927575f730b02cd73fcf
NFerracuti <npferracuti@gmail.com>
2023-09-16 13:57:34 -0400

    complete appointment form functionality

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -9,10 +9,46 @@ import Show from "components/Appointment/Show";
 import Form from "./Form";
 import useVisualMode from "hooks/useVisualMode";
 import Status from "./Status";
+import Confirm from "./Confirm";
+import Error from "./Error";
 
 export default function Appointment(props) {
   // console.log("index props: ", props);
-  const {mode, transition} = useVisualMode(props.interview ? "SHOW" : "EMPTY")
+  //call bookinterview with the form props (student and interview)
+  const EMPTY = "EMPTY";
+  const CREATE = "CREATE";
+  const SHOW = "SHOW";
+  const EDIT = "EDIT";
+  const SAVING = "SAVING";
+  const DELETING = "DELETING";
+  const CONFIRM = "CONFIRM";
+  const ERROR_SAVE = "ERROR_SAVE";
+  const ERROR_DELETE = "ERROR_DELETE";
+
+  const { mode, transition, back } = useVisualMode(props.interview ? SHOW : EMPTY);
+  // console.log("appointment props: ", props);
+
+  function save(name, interviewer) {
+    const interview = {
+      student: name,
+      interviewer
+    };
+
+    transition(SAVING);
+
+    props
+      .bookInterview(props.id, interview)
+      .then(() => transition(SHOW))
+      .catch(error => transition(ERROR_SAVE, true));
+  }
+
+  function destroy() {
+    transition(DELETING, true);
+    props
+      .cancelInterview(props.id)
+      .then(() => transition(EMPTY))
+      .catch(error => transition(ERROR_DELETE, true));
+  }
 
   return (
     <article className="appointment" data-testid="appointment">
@@ -21,30 +57,45 @@ export default function Appointment(props) {
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
-          onDelete={() => transition("CONFIRM")}
-          onEdit={() => transition("EDIT")}
+          onDelete={() => transition(CONFIRM)}
+          onEdit={() => transition(EDIT)}
         />
-      } 
+      }
       {mode === "EMPTY" &&
         <Empty onAdd={() => {
-          transition("CREATE")}
-          } />}
+          transition("CREATE")
+        }
+        } />}
       {mode === "EDIT" &&
-        <Form 
-        interviewers={props.interviewers}
-        onSave={() => transition("SHOW")}
-        onCancel={() => transition("SHOW")}/>
+        <Form
+          name={props.interview.student}
+          interviewer={props.interview.interviewer.id}
+          interviewers={props.interviewers}
+          onSave={() => save()}
+          onCancel={() => back()} />
       }
       {mode === "CREATE" &&
-        <Form 
-        interviewers={props.interviewers}
-        onSave={() => transition("SHOW")}/>
+        <Form
+          interviewers={props.interviewers}
+          onSave={() => save()}
+          onCancel={() => back()}
+          name={props.name} />
       }
-      {mode === "SAVING" &&
-        <Status />
-      }
-      {mode === "DELETING" &&
-        <Status />}
+      {mode === SAVING && <Status message="Saving" />}
+      {mode === DELETING && <Status message="Deleting" />}
+      {mode === CONFIRM && (
+        <Confirm
+          message="Are you sure you would like to delete?"
+          onCancel={back}
+          onConfirm={destroy}
+        />
+      )}
+      {mode === ERROR_SAVE && (
+        <Error message="Could not book appointment." onClose={back} />
+      )}
+      {mode === ERROR_DELETE && (
+        <Error message="Could not cancel appointment." onClose={back} />
+      )}
     </article>
   );
 }
--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,16 +1,26 @@
 import { useState } from "react";
 
 export default function useVisualMode(initialMode) {
-  const [ mode, setMode ] = useState(initialMode);
+  const [history, setHistory] = useState([initialMode]);
 
-  const transition = (newMode) => {
-    console.log("transitioning to ", newMode);
-    setMode(newMode);
+  const transition = (newMode, replace) => {
+    // console.log("transitioning to ", newMode);
+    if (!replace) {
+      setHistory(prev => [...prev, newMode])
+    } else {
+      setHistory(prev => [...prev.slice(0, prev.length - 1), newMode])
+    }
+  }
+  //on click save, transition to saving screen, on error skip saving screen and transition to error
+  // [empty, create, saving, error, create]
+
+  function back() {
+    history.length > 1 && setHistory(prev => [...prev.slice(0, prev.length - 1)]);
   }
 
   return {
-    mode,
-    transition
+    mode: history[history.length - 1],
+    transition,
+    back
   }
 }
-
